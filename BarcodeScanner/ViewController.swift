7e3673a8721b63828525854bd2b79b8b9c5610ee
onl1ner <netv0idrug@mail.ru>
2020-03-21 20:34:16 +0600

    Initial Commit

---
+++ BarcodeScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeScanner
+//
+//  Created by onl1ner onl1ner on 21/03/2020.
+//  Copyright © 2020 onl1ner onl1ner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
